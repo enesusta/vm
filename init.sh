6db9b35bd0ca4f7702942cd8edf52caac204b172
Enes Usta <enesusta@protonmail.com>
2023-03-14 19:19:57 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 user=$(whoami)
+github_user=enesusta
+repo=cheats
 
 sudo add-apt-repository ppa:jonathonf/vim
 sudo apt update
@@ -12,15 +14,18 @@ sudo apt-get install -y ca-certificates \
     lsb-release \
     git-all 
 
-sudo chown -R $user ~/.vim
 
 sudo git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 
+sudo chown -R $user ~/.vim
+
 curl https://raw.githubusercontent.com/enesusta/vimrc/master/.vimrc >> $HOME/.vimrc
 
-sudo vim +PluginInstall +qall
+sudo vim +BundleInstall +qall
 
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 
 brew install navi k0sproject/tap/k0sctl
+
+git clone "https://github.com/${github_user}/${repo}" "$(navi info cheats-path)/${github_user}__${repo}"
 
