f779cc603859277cd5ec67383418d8de77559ad1
Enes Usta <enesusta@protonmail.com>
2024-06-13 00:07:12 +0300

    upgrade version of dockercompose

---
+++ docker-init-armv7.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y ca-certificates \
+    curl \
+    gnupg \
+    lsb-release \
+    git-all \
+    docker.io
+
+wget -O dc https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-armv7 && chmod +x dc && sudo mv dc /usr/bin/docker-compose
+
